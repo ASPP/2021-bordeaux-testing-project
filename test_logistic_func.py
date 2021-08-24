89a1abfc4a7409bc97ef7cc013268853997176ae
ASPP Student <student@localhost>
2021-08-24 12:48:07 +0200

    add test for iterate_f

--- test_logistic_func.py
+++ test_logistic_func.py
@@ -8,7 +8,15 @@ import logistic_func as lf
 def test_logistic_map(x, r, expected):
     result = lf.logistic_map(x, r)
     assert np.isclose(result, expected)
-    
+
+
+@pytest.mark.parametrize("it, x, r, expected", 
+                         [(1, 0.1, 2.2, [0.198]), 
+                          (4, 0.2, 3.4, [0.544, 0.843418, 0.449019, 0.841163]), 
+                          (2, 0.75, 1.7, [0.31875, 0.369152])])
+def test_iterate_f(it, x, r, expected):
+    result = lf.iterate_f(it, x, r)
+    assert np.allclose(result, expected)
 
 
 
