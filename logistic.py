9774341435b0475eb039872f68ba477a16e6b693
ASPP Student <student@localhost>
2021-08-24 15:32:13 +0200

    Add fuzz test for logistic function

--- logistic.py
+++ logistic.py
@@ -9,3 +9,11 @@ def logistic_iteration(it,x,r):
         logistic_out.append(logistic_model(logistic_out[i],r))
 
     return logistic_out[1:]
+
+if __name__ == "__main__":
+    x = [0.3,0.2,0.4]
+    # x = 0.5
+    it = 100
+    r = 1.5
+    log = logistic_iteration(it,x,r)
+    print(log)
--- test_logistic.py
+++ test_logistic.py
@@ -1,9 +1,12 @@
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal, assert_allclose
 import pytest
 from math import isclose
+import numpy as np
+from logistic import logistic_model, logistic_iteration#
 
-from logistic import logistic_model, logistic_iteration
 
+SEED = 42
+random_state = np.random.RandomState(SEED)
 
 @pytest.mark.parametrize("x, r, expected",[(0.1,2.2,0.198),
                             (0.2,3.4,0.544),(0.75,1.7,0.31875)])
@@ -17,3 +20,11 @@ def test_logistic_model(x,r,expected):
 def test_logistic_iteration(it,x,r,expected):
     result = logistic_iteration(it,x,r)
     assert_array_almost_equal(result, expected)
+
+def test_fuzzing_converges():
+    r = 1.5
+    it = 100
+    for _ in range(100):
+        x=random_state.rand(1)
+        result = logistic_iteration(it,x,r)[-1]
+        assert_allclose(result, 1/3)
