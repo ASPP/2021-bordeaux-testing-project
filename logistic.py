a34b08bd61e57f0c90dd8a16f1f5c8ebb901f740
ASPP Student <student@localhost>
2021-08-24 12:44:41 +0200

    Add iterative logistic function and test

--- logistic.py
+++ logistic.py
@@ -1,3 +1,11 @@
 
 def logistic_model(x,r):
     return r*x*(1-x)
+
+def logistic_iteration(it,x,r):
+
+    logistic_out = [x]
+    for i in range(it):
+        logistic_out.append(logistic_model(logistic_out[i],r))
+
+    return logistic_out[1:]
--- test_logistic.py
+++ test_logistic.py
@@ -1,10 +1,19 @@
+from numpy.testing import assert_array_almost_equal
 import pytest
 from math import isclose
 
-from logistic import logistic_model
+from logistic import logistic_model, logistic_iteration
 
 
 @pytest.mark.parametrize("x, r, expected",[(0.1,2.2,0.198),
                             (0.2,3.4,0.544),(0.75,1.7,0.31875)])
 def test_logistic_model(x,r,expected):
     assert isclose(logistic_model(x,r), expected)
+
+
+@pytest.mark.parametrize("it, x, r, expected",[(1,0.1,2.2,[0.198]),
+                            (4,0.2,3.4,[0.544, 0.843418, 0.449019, 0.841163]),
+                            (2,0.75,1.7,[0.31875, 0.369152])])
+def test_logistic_iteration(it,x,r,expected):
+    result = logistic_iteration(it,x,r)
+    assert_array_almost_equal(result, expected)
