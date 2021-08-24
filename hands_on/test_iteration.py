dba3f7774741e702adb070644f660f201106f877
ASPP Student <student@localhost>
2021-08-24 14:48:51 +0200

    klnkh

---
+++ hands_on/test_iteration.py
@@ -0,0 +1,42 @@
+from logistic_map import *
+import pytest
+import numpy as np
+
+
+x=[0.1, 0.2, 0.75]
+r=[2.2, 3.4, 1.7]
+it=[1,4,2]
+
+
+@pytest.mark.parametrize('x,r,expected', 
+	[
+	(0.1, 2.2, 0.198),
+	(0.2, 3.4, 0.544),
+	(0.75, 1.7, 0.31875),])
+	
+def test_f(x,r,expected):
+	result = log_nap(r,x)
+	assert np.isclose(result,expected)
+	
+
+
+@pytest.mark.parametrize('x,r,it,expected', 
+	[
+	(0.1, 2.2, 1, 0.198),
+	(0.2, 3.4, 4, 0.544),
+	(0.75, 1.7, 2, 0.31875),])
+	
+	
+	
+def test_log_nap(x,r,it,expected):
+    #when
+    result = iterate_f(it=it, fn1=log_nap, r=r, x=x)
+    #then
+    if len(result)==1: 
+        expected = [0.198]
+    elif len(result)==4:
+        expected = [0.544, 0.843418, 0.449019, 0.841163]
+    elif len(result)==2:
+        expected = [0.31875, 0.369152]
+    assert np.all(np.isclose(expected,result))
+
